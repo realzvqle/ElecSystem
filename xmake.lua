1490fccbe09122c647d9b391455fb365af3f96fa
realzvqle <realzvqle@gmail.com>
2024-12-23 18:47:42 -0600

    fix

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,10 @@
-set_project("projgeopol")
+set_project("elec")
 
 set_xmakever("2.6.0")
 
 set_languages("c99")
 
-target("projgeopol")
+target("elec")
     set_kind("binary")  
     add_files("src/*.c")
     add_files("src/**/*.c") 
